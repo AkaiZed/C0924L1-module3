f8e55468261eb4cc59ea6ab0af2eff7a9d7b089d
AlexZedred <wtf0888506598@gmail.com>
2025-02-07 21:09:13 +0700

    Nap bai

---
+++ DataBase/ss1_tong_quan_csdl.sql
@@ -0,0 +1,31 @@
+create database student_management;
+use student_management;
+
+CREATE TABLE Class (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(100) NOT NULL
+);
+CREATE TABLE Teacher (
+    id INT PRIMARY KEY,
+    `name` VARCHAR(100) NOT NULL,
+    age INT NOT NULL,
+    country VARCHAR(255) NOT NULL
+);
+insert into Class(`name`) values ('Lớp 1'),
+('Lớp 2'),
+('Lớp 3');
+insert into Teacher (id,`name`,age,country) values(12, 'Phan Thanh Hà', 35, 'Quảng Ngãi'),
+(11, 'Nguyễn Thị Ngân', 28, 'Quảng Nam'),
+(6, 'Phạm Thị Hương',45,'Hà Nam');
+SELECT 
+    *
+FROM
+    Class;
+SELECT 
+    *
+FROM
+    Teacher;
+    -- drop table Teacher; --
+/* DELETE FROM Teacher 
+ORDER BY id DESC 
+LIMIT 1;*/
