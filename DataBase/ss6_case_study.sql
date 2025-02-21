1652c9884a15ae27ed68a5cd3bbf291a424ca30b
AlexZedred <wtf0888506598@gmail.com>
2025-02-21 12:45:26 +0700

    Nap bai

---
+++ DataBase/ss6_case_study.sql
@@ -0,0 +1,258 @@
+CREATE DATABASE casestudy_sql;
+USE casestudy_sql;
+
+CREATE TABLE vi_tri (
+    ma_vi_tri INT PRIMARY KEY,
+    ten_vi_tri VARCHAR(45) NOT NULL
+);
+INSERT INTO vi_tri (ma_vi_tri, ten_vi_tri) VALUES
+(1, 'Quản Lý'),
+(2, 'Nhân Viên');
+CREATE TABLE trinh_do (
+    ma_trinh_do INT PRIMARY KEY,
+    ten_trinh_do VARCHAR(45) NOT NULL
+);
+INSERT INTO trinh_do (ma_trinh_do, ten_trinh_do) VALUES
+(1, 'Trung Cấp'),
+(2, 'Cao Đẳng'),
+(3, 'Đại Học'),
+(4, 'Sau Đại Học');
+CREATE TABLE bo_phan (
+    ma_bo_phan INT PRIMARY KEY,
+    ten_bo_phan VARCHAR(45) NOT NULL
+);
+INSERT INTO bo_phan (ma_bo_phan, ten_bo_phan) VALUES
+(1, 'Sale-Marketing'),
+(2, 'Hành chính'),
+(3, 'Phục vụ'),
+(4, 'Quản lý');
+CREATE TABLE nhan_vien (
+    ma_nhan_vien INT PRIMARY KEY,
+    ho_ten VARCHAR(45) NOT NULL,
+    ngay_sinh DATE NOT NULL,
+    so_cmnd VARCHAR(45) NOT NULL,
+    luong DOUBLE NOT NULL,
+    so_dien_thoai VARCHAR(45) NOT NULL,
+    email VARCHAR(45) NOT NULL,
+    dia_chi VARCHAR(255) NOT NULL,
+    ma_vi_tri INT NOT NULL,
+    ma_trinh_do INT NOT NULL,
+    ma_bo_phan INT NOT NULL,
+    FOREIGN KEY (ma_vi_tri) REFERENCES vi_tri(ma_vi_tri),
+    FOREIGN KEY (ma_trinh_do) REFERENCES trinh_do(ma_trinh_do),
+    FOREIGN KEY (ma_bo_phan) REFERENCES bo_phan(ma_bo_phan)
+);
+INSERT INTO nhan_vien (ma_nhan_vien, ho_ten, ngay_sinh, so_cmnd, luong, so_dien_thoai, email, dia_chi, ma_vi_tri, ma_trinh_do, ma_bo_phan) VALUES
+(1, 'Nguyễn Văn An', '1970-11-07', '456231786', 10000000, '0901234121', 'annguyen@gmail.com', '295 Nguyễn Tất Thành, Đà Nẵng', 1, 3, 1),
+(2, 'Lê Văn Bình', '1997-04-09', '654231234', 7000000, '0934212314', 'binhlv@gmail.com', '22 Yên Bái, Đà Nẵng', 1, 2, 2),
+(3, 'Hồ Thị Yến', '1995-12-12', '999231723', 14000000, '0412352315', 'thiyen@gmail.com', 'K234/11 Điện Biên Phủ, Gia Lai', 1, 3, 2),
+(4, 'Võ Công Toàn', '1980-04-04', '123231365', 17000000, '0374443232', 'toan0404@gmail.com', '77 Hoàng Diệu, Quảng Trị', 1, 4, 4),
+(5, 'Nguyễn Bình Phát', '1999-12-29', '454363232', 6000000, '0920341231', 'phatphat@gmail.com', '43 Yên Bái, Đà Nẵng', 2, 3, 1),
+(6, 'Khúc Nguyễn An Nghi', '2000-11-08', '964542311', 7000000, '0978652313', 'annghi20@gmail.com', '294 Nguyễn Tất Thành, Đà Nẵng', 2, 3, 2),
+(7, 'Nguyễn Hữu Hà', '1993-01-01', '534323231', 8000000, '0941234553', 'nhh0101@gmail.com', '4 Nguyễn Chí Thanh, Huế', 2, 4, 3),
+(8, 'Nguyễn Hà Đông', '1989-09-03', '234414123', 9000000, '0642123111', 'donghanguyen@gmail.com', '111 Hùng Vương, Hà Nội', 2, 4, 4),
+(9, 'Tống Hoàng', '1982-09-03', '256781231', 6000000, '0245144444', 'hoangtong@gmail.com', '213 Hàm Nghi, Đà Nẵng', 2, 3, 4),
+(10, 'Nguyễn Công Đạo', '1994-01-08', '755434343', 8000000, '0988767111', 'nguyencongdao12@gmail.com', '6 Hoà Khánh, Đồng Nai', 2, 3, 2);
+CREATE TABLE loai_khach (
+    ma_loai_khach INT PRIMARY KEY,
+    ten_loai_khach VARCHAR(45) NOT NULL
+);
+INSERT INTO loai_khach (ma_loai_khach, ten_loai_khach) VALUES 
+(1, 'Diamond'),
+(2, 'Platinium'),
+(3, 'Gold'),
+(4, 'Silver'),
+(5, 'Member');
+CREATE TABLE khach_hang (
+    ma_khach_hang INT PRIMARY KEY,
+    ma_loai_khach INT NOT NULL,
+    ho_ten VARCHAR(45) NOT NULL,
+    ngay_sinh DATE NOT NULL,
+    gioi_tinh BIT(1) NOT NULL,
+    so_cmnd VARCHAR(45) NOT NULL,
+    so_dien_thoai VARCHAR(45) NOT NULL,
+    email VARCHAR(45) NOT NULL,
+    dia_chi VARCHAR(255) NOT NULL,
+    FOREIGN KEY (ma_loai_khach) REFERENCES loai_khach(ma_loai_khach)
+);
+INSERT INTO khach_hang (ma_khach_hang, ho_ten, ngay_sinh, gioi_tinh, so_cmnd, so_dien_thoai, email, dia_chi, ma_loai_khach) VALUES
+(1, 'Nguyễn Thị Hảo', '1970-11-07', 0, '643431213', '0945423362', 'thhao07@gmail.com', '23 Nguyễn Hoàng, Đà Nẵng', 5),
+(2, 'Phạm Xuân Diệu', '1992-08-08', 1, '865432123', '0954333333', 'xuandieu92@gmail.com', 'K77/22 Thái Phiên, Quảng Trị', 3),
+(3, 'Trương Đình Nghệ', '1990-02-27', 1, '438645199', '0372313122', 'nghenhan2702@gmail.com', 'K323/12 Ông Ích Khiêm, Vinh', 2),
+(4, 'Dương Văn Quân', '1981-07-08', 1, '543432111', '0490039241', 'duongquan@gmail.com', 'K453/12 Lê Lợi, Đà Nẵng', 1),
+(5, 'Hoàng Trần Nhi Nhi', '1995-12-09', 0, '795453345', '0312454678', 'nhinhi123@gmail.com', '224 Lý Thái Tổ, Gia Lai', 4),
+(6, 'Tôn Nữ Mộc Châu', '2005-12-06', 0, '732434215', '0988888844', 'tonnuchau@gmail.com', '37 Yên Thế, Đà Nẵng', 4),
+(7, 'Nguyễn Mỹ Kim', '1984-04-08', 0, '965465433', '0912345698', 'kimcuong84@gmail.com', 'K123/45 Lê Lợi, Hồ Chí Minh', 3),
+(8, 'Nguyễn Thị Hảo', '1999-04-08', 0, '965654633', '0935456789', 'haohxa99@gmail.com', '55 Nguyễn Văn Linh, Kon Tum', 1),
+(9, 'Trần Đại Danh', '1994-07-01', 1, '432341235', '0643343433', 'danhhai99@gmail.com', '24 Lý Thường Kiệt, Quảng Ngãi', 5),
+(10, 'Nguyễn Tâm Đắc', '1989-07-01', 1, '344343432', '0987654231', 'dactam@gmail.com', '22 Ngô Quyền, Đà Nẵng', 2);
+CREATE TABLE kieu_thue (
+    ma_kieu_thue INT PRIMARY KEY,
+    ten_kieu_thue VARCHAR(45) NOT NULL
+);
+INSERT INTO kieu_thue (ma_kieu_thue, ten_kieu_thue) VALUES
+(1, 'Năm'),
+(2, 'Tháng'),
+(3, 'Ngày'),
+(4, 'Giờ');
+CREATE TABLE loai_dich_vu (
+    ma_loai_dich_vu INT PRIMARY KEY,
+    ten_loai_dich_vu VARCHAR(45) NOT NULL
+);
+INSERT INTO loai_dich_vu (ma_loai_dich_vu, ten_loai_dich_vu) VALUES
+(1, 'Villa'),
+(2, 'House'),
+(3, 'Room');
+CREATE TABLE dich_vu (
+    ma_dich_vu INT PRIMARY KEY,
+    ten_dich_vu VARCHAR(45) NOT NULL,
+    dien_tich INT NOT NULL,
+    chi_phi_thue DOUBLE NOT NULL,
+    so_nguoi_toi_da INT NOT NULL,
+    ma_kieu_thue INT NOT NULL,
+    ma_loai_dich_vu INT NOT NULL,
+    tieu_chuan_phong VARCHAR(45) NOT NULL,
+    mo_ta_tien_nghi_khac VARCHAR(255),
+    dien_tich_ho_boi DOUBLE,
+    so_tang INT,
+    FOREIGN KEY (ma_kieu_thue) REFERENCES kieu_thue(ma_kieu_thue),
+    FOREIGN KEY (ma_loai_dich_vu) REFERENCES loai_dich_vu(ma_loai_dich_vu)
+);
+INSERT INTO dich_vu (ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, so_nguoi_toi_da, tieu_chuan_phong, mo_ta_tien_nghi_khac, dien_tich_ho_boi, so_tang, ma_kieu_thue, ma_loai_dich_vu) VALUES
+(1, 'Villa Beach Front', 25000, 10000000, 10, 'vip', 'Có hồ bơi', 500, 4, 3, 1),
+(2, 'House Princess 01', 14000, 5000000, 7, 'vip', 'Có thêm bếp nướng', NULL, 3, 2, 2),
+(3, 'Room Twin 01', 5000, 1000000, 2, 'normal', 'Có thể', NULL, NULL, 4, 3),
+(4, 'Villa No Beach Front', 22000, 9000000, 8, 'normal', 'Có hồ bơi', 300, 3, 3, 1),
+(5, 'House Princess 02', 10000, 4000000, 5, 'normal', 'Có thêm bếp nướng', NULL, 2, 3, 2),
+(6, 'Room Twin 02', 3000, 9000000, 2, 'normal', 'Có thể', NULL, NULL, 4, 3);
+CREATE TABLE hop_dong (
+    ma_hop_dong INT PRIMARY KEY,
+    ngay_lam_hop_dong DATETIME NOT NULL,
+    ngay_ket_thuc DATETIME NOT NULL,
+    tien_dat_coc DOUBLE NOT NULL,
+    ma_nhan_vien INT NOT NULL,
+    ma_khach_hang INT NOT NULL,
+    ma_dich_vu INT NOT NULL,
+    FOREIGN KEY (ma_nhan_vien) REFERENCES nhan_vien(ma_nhan_vien),
+    FOREIGN KEY (ma_khach_hang) REFERENCES khach_hang(ma_khach_hang),
+    FOREIGN KEY (ma_dich_vu) REFERENCES dich_vu(ma_dich_vu)
+);
+INSERT INTO hop_dong (ma_hop_dong, ngay_lam_hop_dong, ngay_ket_thuc, tien_dat_coc, ma_nhan_vien, ma_khach_hang, ma_dich_vu) VALUES
+(1, '2020-12-08', '2020-12-08', 0, 3, 1, 3),
+(2, '2020-07-14', '2020-07-21', 200000, 7, 3, 1),
+(3, '2021-03-15', '2021-03-17', 50000, 3, 4, 2),
+(4, '2021-01-14', '2021-01-18', 100000, 7, 5, 5),
+(5, '2021-07-14', '2021-07-15', 0, 7, 2, 6),
+(6, '2021-06-01', '2021-06-03', 0, 7, 7, 6),
+(7, '2021-09-02', '2021-09-05', 100000, 7, 4, 4),
+(8, '2021-06-17', '2021-06-18', 150000, 3, 4, 1),
+(9, '2020-11-19', '2020-11-19', 0, 3, 4, 3),
+(10, '2021-04-12', '2021-04-14', 0, 10, 3, 5),
+(11, '2021-04-25', '2021-04-25', 0, 2, 2, 1),
+(12, '2021-05-25', '2021-05-27', 0, 7, 10, 1);
+CREATE TABLE dich_vu_di_kem (
+    ma_dich_vu_di_kem INT PRIMARY KEY,
+    ten_dich_vu_di_kem VARCHAR(45),
+    gia DOUBLE,
+    don_vi VARCHAR(10),
+    trang_thai VARCHAR(45)
+);
+INSERT INTO dich_vu_di_kem (ma_dich_vu_di_kem, ten_dich_vu_di_kem, gia, don_vi, trang_thai) VALUES
+(1, 'Karaoke', 10000, 'giờ', 'Tiện nghi, hiện tại'),
+(2, 'Thuê xe máy', 10000, 'chiếc', 'Hỏng 1 xe'),
+(3, 'Thuê xe đạp', 20000, 'chiếc', 'Tốt'),
+(4, 'Buffet buổi sáng', 15000, 'suất', 'Đầy đủ đồ ăn, tráng miệng'),
+(5, 'Buffet buổi trưa', 90000, 'suất', 'Đầy đủ đồ ăn, tráng miệng'),
+(6, 'Buffet buổi tối', 16000, 'suất', 'Đầy đủ đồ ăn, tráng miệng');
+CREATE TABLE hop_dong_chi_tiet (
+    ma_hop_dong_chi_tiet INT PRIMARY KEY,
+    ma_hop_dong INT,
+    ma_dich_vu_di_kem INT,
+    so_luong INT,
+    FOREIGN KEY (ma_hop_dong) REFERENCES hop_dong(ma_hop_dong),
+    FOREIGN KEY (ma_dich_vu_di_kem) REFERENCES dich_vu_di_kem(ma_dich_vu_di_kem)
+);
+INSERT INTO hop_dong_chi_tiet (ma_hop_dong_chi_tiet, so_luong, ma_hop_dong, ma_dich_vu_di_kem) VALUES
+(1, 5, 2, 4),
+(2, 8, 2, 5),
+(3, 15, 2, 6),
+(4, 1, 3, 1),
+(5, 11, 3, 2),
+(6, 1, 1, 3),
+(7, 2, 1, 2),
+(8, 2, 12, 2);
+-- DROP DATABASE IF EXISTS casestudy_sql;
+
+SELECT * FROM nhan_vien 
+WHERE (ho_ten LIKE 'H%' OR ho_ten LIKE 'T%' OR ho_ten LIKE 'K%') 
+AND LENGTH(ho_ten) <= 15;
+
+SELECT * FROM khach_hang
+WHERE TIMESTAMPDIFF(YEAR, ngay_sinh, CURDATE()) BETWEEN 18 AND 50
+AND (dia_chi LIKE '%Đà Nẵng%' OR dia_chi LIKE '%Quảng Trị%');
+
+SELECT khach_hang.ma_khach_hang, khach_hang.ho_ten, COUNT(hop_dong.ma_hop_dong) AS so_lan_dat_phong
+FROM khach_hang
+JOIN loai_khach ON khach_hang.ma_loai_khach = loai_khach.ma_loai_khach
+JOIN hop_dong ON khach_hang.ma_khach_hang = hop_dong.ma_khach_hang
+WHERE loai_khach.ten_loai_khach = 'Diamond'
+GROUP BY khach_hang.ma_khach_hang, khach_hang.ho_ten
+ORDER BY so_lan_dat_phong ASC;
+
+SELECT kh.ma_khach_hang, kh.ho_ten, lk.ten_loai_khach, hd.ma_hop_dong, dv.ten_dich_vu, 
+       hd.ngay_lam_hop_dong, hd.ngay_ket_thuc, 
+       (dv.chi_phi_thue + IFNULL(SUM(hdct.so_luong * dvdk.gia), 0)) AS tong_tien
+FROM khach_hang kh
+LEFT JOIN loai_khach lk ON kh.ma_loai_khach = lk.ma_loai_khach
+LEFT JOIN hop_dong hd ON kh.ma_khach_hang = hd.ma_khach_hang
+LEFT JOIN dich_vu dv ON hd.ma_dich_vu = dv.ma_dich_vu
+LEFT JOIN hop_dong_chi_tiet hdct ON hd.ma_hop_dong = hdct.ma_hop_dong
+LEFT JOIN dich_vu_di_kem dvdk ON hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
+GROUP BY kh.ma_khach_hang, kh.ho_ten, lk.ten_loai_khach, hd.ma_hop_dong, dv.ten_dich_vu, hd.ngay_lam_hop_dong, hd.ngay_ket_thuc, dv.chi_phi_thue
+ORDER BY kh.ma_khach_hang;
+
+SELECT dv.ma_dich_vu, dv.ten_dich_vu, dv.dien_tich, dv.chi_phi_thue, ldv.ten_loai_dich_vu
+FROM dich_vu dv
+JOIN loai_dich_vu ldv ON dv.ma_loai_dich_vu = ldv.ma_loai_dich_vu
+LEFT JOIN hop_dong hd ON dv.ma_dich_vu = hd.ma_dich_vu
+      AND YEAR(hd.ngay_lam_hop_dong) = 2021
+      AND MONTH(hd.ngay_lam_hop_dong) BETWEEN 1 AND 3
+WHERE hd.ma_hop_dong IS NULL;
+
+SELECT dv.ma_dich_vu, dv.ten_dich_vu, dv.dien_tich, dv.so_nguoi_toi_da, dv.chi_phi_thue, ldv.ten_loai_dich_vu
+FROM dich_vu dv
+JOIN loai_dich_vu ldv ON dv.ma_loai_dich_vu = ldv.ma_loai_dich_vu
+WHERE dv.ma_dich_vu IN (
+    SELECT DISTINCT hd_2020.ma_dich_vu
+    FROM hop_dong hd_2020
+    WHERE YEAR(hd_2020.ngay_lam_hop_dong) = 2020
+)
+AND dv.ma_dich_vu NOT IN (
+    SELECT DISTINCT hd_2021.ma_dich_vu
+    FROM hop_dong hd_2021
+    WHERE YEAR(hd_2021.ngay_lam_hop_dong) = 2021
+);
+
+-- Cách 1
+SELECT DISTINCT ho_ten FROM khach_hang;
+-- Cách 2
+SELECT ho_ten FROM khach_hang GROUP BY ho_ten;
+-- Cách 3
+SELECT ho_ten FROM (
+    SELECT ho_ten, ROW_NUMBER() OVER (PARTITION BY ho_ten ORDER BY ma_khach_hang) AS row_num
+    FROM khach_hang
+) AS temp
+WHERE row_num = 1;
+
+SELECT MONTH(hd.ngay_lam_hop_dong) AS thang, COUNT(DISTINCT hd.ma_khach_hang) AS so_khach_hang
+FROM hop_dong hd
+WHERE YEAR(hd.ngay_lam_hop_dong) = 2021
+GROUP BY thang
+ORDER BY thang;
+
+SELECT hd.ma_hop_dong, hd.ngay_lam_hop_dong, hd.ngay_ket_thuc, hd.tien_dat_coc,
+       IFNULL(SUM(hdct.so_luong), 0) AS so_luong_dich_vu_di_kem
+FROM hop_dong hd
+LEFT JOIN hop_dong_chi_tiet hdct ON hd.ma_hop_dong = hdct.ma_hop_dong
+GROUP BY hd.ma_hop_dong, hd.ngay_lam_hop_dong, hd.ngay_ket_thuc, hd.tien_dat_coc
+ORDER BY hd.ma_hop_dong;
